d3274d170f0977350f1465341fc70b45b2c4cec3
georgemarynych <Egor.Marynych@waveaccess>
2024-11-06 14:27:33 +0300

    добавил MainActivity.kt

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,4 +1,47 @@
 package com.example.weatherapp
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.weatherapp.navigation.StartupCoordinator
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.androidx.AppNavigator
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity: AppCompatActivity(R.layout.activity_main) {
+
+    @Inject
+    lateinit var startupCoordinator: StartupCoordinator
+
+    @Inject
+    lateinit var navigatorHolder: NavigatorHolder
+
+    private val navigator = AppNavigator(this, R.id.abFlMainContainer)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Устанавливаем сплэш-скрин
+       installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        // Переход на следующий экран
+        startupCoordinator.showWeatherInfoScreen()
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        navigatorHolder.removeNavigator()
+        super.onPause()
+    }
 }
